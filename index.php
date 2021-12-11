3bb35fab5569686cfd6e875ad9992d5b3129a0b1
anueljhay <84647597+anueljhay@users.noreply.github.com>
2021-12-11 11:44:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
-<meta property="og:image" content="https://www.youtube.com/img/desktop/yt_1200.png" />
-<meta property="fb:app_id" content="87741124305" />
+<?php
 
-<p></p><div class="separator" style="clear: both; text-align: center;"><a href="https://ovionthd.agilecrm.com/landing/5621996090621952" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="508" data-original-width="877" height="370" src="https://blogger.googleusercontent.com/img/a/AVvXsEhZvM_R77I4DZrMifCQclQRnPhWg-e_Hi_QjRW0u_WDPiPtDOVEEYP1sPLMZf1KqhAeCJtjIRfhY6dS8JCxs1McvwrU8iIFkhIx0S9_CVXVIEes0NKx84UVU35ddBM3ju4e1V_QpfXa3E1E5UWfSSRN9JHaSlNbmdp32frVz-ZDcNUEM_1FTFlwVueePQ=w640-h370" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://ovionthd.agilecrm.com/landing/5621996090621952" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="511" data-original-width="878" height="372" src="https://blogger.googleusercontent.com/img/a/AVvXsEixIrtYzsNWau02lIHVedaEU_Na6hG9DKEqcA_Z7ttsrvz8thuhZKaZNl60Md0ww19jcZjhJfV92o8ODfFsqiu_NoTGJk0wSazBXXqhfkLLG4ghoKI3MkmbIdV4VAi2FWVaWGo5eVsoiFCtP74uuU1X15UqY_FTypBm4eZN1wCBCT6eE_6DYFmMQwm-Ag=w640-h372" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://ovionthd.agilecrm.com/landing/5621996090621952" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="509" data-original-width="878" height="372" src="https://blogger.googleusercontent.com/img/a/AVvXsEjWIQhC2lne_jkv2GuORpGq4-MZYDeWmIyNjYJ4Y-o9K4S2RFPo3DtzURUBAIwcF8DxPP9jXMEQlfXr-HLcU_4CJZEFwvVwHfXJf-xaLgVwCuD5wm2YD7ZvB3Adurmmj4TxjCGNhDi8JSBDc1iAY07lYBEYqeqSuluaPFlQLdqysvxg8VUdLdR7j2OEkA=w640-h372" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><div class="separator" style="clear: both; text-align: center;"><a href="https://ovionthd.agilecrm.com/landing/5621996090621952" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="509" data-original-width="878" height="372" src="https://blogger.googleusercontent.com/img/a/AVvXsEiCI2iV9bQMUBDqYIUtVfcm0nG6DzzYaa2d9HgJh1jnkD4nzCMwdkXsvh1uEr_rW8tjC3rTiu2npHK4rmsv5GbP1XIuAZT9tXLPMD7TOy1o5mOW15ARpUQ1M2LAOvaZHtJiuvpd1Gy23PrWK4RW5dkPUc6VUu11mqxNUVMmZfhynscOjT8mnGfTOKqnKQ=w640-h372" width="640" /></a></div><br /><a href="https://ovionthd.agilecrm.com/landing/5621996090621952" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="508" data-original-width="874" height="372" src="https://blogger.googleusercontent.com/img/a/AVvXsEgz9sXUoCKV1jFal1hQKQr4epAEnaEV-u6EHplxiXckF6pdRyzv1PwTxrPPM_OspW_p2Z5Ogg9fhXB8rgnyS2WOxkEW6UcL70UhhmQr4kb1tToczbvMrIW8xqfP-INj549APz-IuXFC6QwiYQfsLoHQF9QBYpa1o116wFI-8MwZa2aI7nL7jesNbxSrZg=w640-h372" width="640" /></a></div><br />&nbsp;<p></p><script src="//widgets.amung.us/small.js" type="text/javascript"></script><script type="text/javascript">WAU_small('ghjfiii')</script>
+// Genero: ProgramasProgramacion.com
+echo "<meta property=\"og:image\" content=\"https://www.youtube.com/img/desktop/yt_1200.png\" />\n";
+echo "<meta property=\"fb:app_id\" content=\"87741124305\" />\n";
+echo "\n";
+echo "<p></p><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://ovionthd.agilecrm.com/landing/5621996090621952\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"508\" data-original-width=\"877\" height=\"370\" src=\"https://blogger.googleusercontent.com/img/a/AVvXsEhZvM_R77I4DZrMifCQclQRnPhWg-e_Hi_QjRW0u_WDPiPtDOVEEYP1sPLMZf1KqhAeCJtjIRfhY6dS8JCxs1McvwrU8iIFkhIx0S9_CVXVIEes0NKx84UVU35ddBM3ju4e1V_QpfXa3E1E5UWfSSRN9JHaSlNbmdp32frVz-ZDcNUEM_1FTFlwVueePQ=w640-h370\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://ovionthd.agilecrm.com/landing/5621996090621952\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"511\" data-original-width=\"878\" height=\"372\" src=\"https://blogger.googleusercontent.com/img/a/AVvXsEixIrtYzsNWau02lIHVedaEU_Na6hG9DKEqcA_Z7ttsrvz8thuhZKaZNl60Md0ww19jcZjhJfV92o8ODfFsqiu_NoTGJk0wSazBXXqhfkLLG4ghoKI3MkmbIdV4VAi2FWVaWGo5eVsoiFCtP74uuU1X15UqY_FTypBm4eZN1wCBCT6eE_6DYFmMQwm-Ag=w640-h372\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://ovionthd.agilecrm.com/landing/5621996090621952\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"509\" data-original-width=\"878\" height=\"372\" src=\"https://blogger.googleusercontent.com/img/a/AVvXsEjWIQhC2lne_jkv2GuORpGq4-MZYDeWmIyNjYJ4Y-o9K4S2RFPo3DtzURUBAIwcF8DxPP9jXMEQlfXr-HLcU_4CJZEFwvVwHfXJf-xaLgVwCuD5wm2YD7ZvB3Adurmmj4TxjCGNhDi8JSBDc1iAY07lYBEYqeqSuluaPFlQLdqysvxg8VUdLdR7j2OEkA=w640-h372\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://ovionthd.agilecrm.com/landing/5621996090621952\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"509\" data-original-width=\"878\" height=\"372\" src=\"https://blogger.googleusercontent.com/img/a/AVvXsEiCI2iV9bQMUBDqYIUtVfcm0nG6DzzYaa2d9HgJh1jnkD4nzCMwdkXsvh1uEr_rW8tjC3rTiu2npHK4rmsv5GbP1XIuAZT9tXLPMD7TOy1o5mOW15ARpUQ1M2LAOvaZHtJiuvpd1Gy23PrWK4RW5dkPUc6VUu11mqxNUVMmZfhynscOjT8mnGfTOKqnKQ=w640-h372\" width=\"640\" /></a></div><br /><a href=\"https://ovionthd.agilecrm.com/landing/5621996090621952\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"508\" data-original-width=\"874\" height=\"372\" src=\"https://blogger.googleusercontent.com/img/a/AVvXsEgz9sXUoCKV1jFal1hQKQr4epAEnaEV-u6EHplxiXckF6pdRyzv1PwTxrPPM_OspW_p2Z5Ogg9fhXB8rgnyS2WOxkEW6UcL70UhhmQr4kb1tToczbvMrIW8xqfP-INj549APz-IuXFC6QwiYQfsLoHQF9QBYpa1o116wFI-8MwZa2aI7nL7jesNbxSrZg=w640-h372\" width=\"640\" /></a></div><br /> <p></p><script src=\"//widgets.amung.us/small.js\" type=\"text/javascript\"></script><script type=\"text/javascript\">WAU_small('ghjfiii')</script>\n";
+echo "\n";
+echo "<script type='text/javascript'>\n";
+echo "	document.oncontextmenu = function(){return false}\n";
+echo "</script>\n";
+echo "</script>\n";
+echo "\n";
+echo "\n";
+echo "\n";
+echo "\n";
+echo "<script type=\"text/javascript\">\n";
+echo "//<![CDATA[\n";
+echo " function h(r, a) { var t = \"\"; if (\"mix\" == a) var h = \n";
+echo "\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\"; else var h = \n";
+echo "\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\"; for (var n = 0; r > n; n++) t += h.charAt(Math.floor(Math.random() * \n";
+echo "h.length)); return t };\n";
+echo "\n";
+echo "        var device = navigator.userAgent\n";
+echo "        \n";
+echo "        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))\n";
+echo "        {\n";
+echo "         \n";
+echo "        }else\n";
+echo "{\n";
+echo "window.location = \"https://www.youtube.com/?/\"\n";
+echo "}\n";
+echo "//]]>\n";
+echo "</script>\n";
 
-<script type='text/javascript'>
-	document.oncontextmenu = function(){return false}
-</script>
-</script>
-
-
-
-
-<script type="text/javascript">
-//<![CDATA[
- function h(r, a) { var t = ""; if ("mix" == a) var h = 
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; else var h = 
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; for (var n = 0; r > n; n++) t += h.charAt(Math.floor(Math.random() * 
-h.length)); return t };
-
-        var device = navigator.userAgent
-        
-        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
-        {
-         
-        }else
-{
-window.location = "https://www.youtube.com/?/"
-}
-//]]>
-</script>
+?>
